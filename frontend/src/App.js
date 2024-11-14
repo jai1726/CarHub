5b3eda2d0919fd8f1f8ec4efdd93f276b1519df7
jai1726 <jagadeeshdende@gmail.com>
2024-11-14 17:38:10 +0530

    Frotend Setup Ussing React and Tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="bg-yellow-400 text-5">
+    React App zfbdfb
+      </div>
+    </div>
+  );
+}
+
+export default App;
