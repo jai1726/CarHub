f1301b661bc5344c2b93eb127f4d6a9250646d75
jai1726 <jagadeeshdende@gmail.com>
2024-11-15 15:53:22 +0530

    cleared unnecessary line

--- backend/routes/Car.Routes.js
+++ backend/routes/Car.Routes.js
@@ -3,18 +3,21 @@ const authenticateToken = require('../middleware/auth');
 const addCar=require('../controllers/addCar');
 const updateCar=require('../controllers/updateCar');
 const deleteCar=require('../controllers/deleteCar');
+const searchResult=require('../controllers/searchResults');
+const myCars=require('../controllers/myCars');
+// const getCarById=require('../controllers/getCarById');
 const multer = require('multer');
 const { storage } = require('../middleware/multerAuth'); 
-// const addToWishlist=require('../controllers/addToWishlist');
-// const wishList=require('../controllers/wishlist');
-// const posts=require('../controllers/posts');
 const upload = multer({ storage });
 const carRouter=express.Router();
 
-
 carRouter.post('/addCar',authenticateToken,upload.array('images',10),addCar);
 carRouter.delete('/delete/:id',authenticateToken,deleteCar);
-carRouter.post('/updateCar/:id',authenticateToken,upload.array('images',10),updateCar);
+carRouter.put('/updateCar/:id',authenticateToken,upload.array('images',10),updateCar);
+carRouter.get('/search/',authenticateToken,searchResult);
+carRouter.get('/myCars',authenticateToken,myCars);
+// carRouter.get('/:id',authenticateToken,getCarById);
+
 
 
 module.exports=carRouter;
