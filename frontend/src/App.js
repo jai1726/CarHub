d574a5bc52ba28c3e74737f43aefa2d432daef1d
jai1726 <jagadeeshdende@gmail.com>
2024-11-15 15:57:41 +0530

    implemented Routing for Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+// import Navbar from './Components/Navbar';
+import HomePage from './Components/HomePage';
+import AddCar from './Components/addCar';
+import SearchResults from './Components/SearchResults';
+import MyCars from './Components/MyCars';
+import CarDetails from './Components/CarDetails'; 
 function App() {
+
   return (
-    <div className="App">
-    <div className="bg-yellow-400 text-5">
-    React App zfbdfb
-      </div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+           <Route path="/addCar" element={<AddCar/>}/>
+           <Route path="/search" element={<SearchResults/>}/>
+           <Route path="/MyCars" element={<MyCars/>}/>
+           <Route path="/carDetails/:carId" element={<CarDetails/>} />
+        </Routes>
+      </BrowserRouter>
+     
+  
   );
 }
 
